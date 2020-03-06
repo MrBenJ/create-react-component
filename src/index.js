2d6c2a43103a768eb87f450eef2ac96360d1a92b
Ben Junya <bjunya@gmail.com>
2020-03-05 21:33:13 -0800

    Initial Commit with setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyComponent = props => {
+
+};
+
+export default MyComponent;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: '/',
+    libraryTarget: 'commonjs2'
+  },
+  mode: process.env.NODE_ENV || 'production',
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      }
+    ]
+  },
+  externals: {
+    'react': 'commonjs react'
+  }
+};
